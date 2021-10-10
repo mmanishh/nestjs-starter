ae41027c07fdc57d3c5609d23326fd255c35fbf5
zsevic <zsevic95@gmail.com>
2021-10-10 12:38:50 +0200

    Improve running migrations

--- src/common/config/database.ts
+++ src/common/config/database.ts
@@ -1,13 +1,22 @@
 import path from 'path';
 import { registerAs } from '@nestjs/config';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 
-export default registerAs('database', () => ({
-  entities: [path.join(__dirname, '/../../../**/*.entity.{js,ts}')],
-  keepConnectionAlive: true,
-  logging: false,
-  migrations: ['dist/database/migrations/*{.ts,.js}'],
-  migrationsTableName: 'migrations',
-  synchronize: false,
-  type: 'postgres',
-  url: process.env.DATABASE_URL,
-}));
+interface ConnectionOptions extends PostgresConnectionOptions {
+  keepConnectionAlive: boolean;
+}
+
+export default registerAs(
+  'database',
+  (): ConnectionOptions => ({
+    entities: [path.join(__dirname, '/../../../**/*.entity.{js,ts}')],
+    keepConnectionAlive: true,
+    logging: false,
+    migrations: ['dist/database/migrations/*{.ts,.js}'],
+    migrationsRun: true,
+    migrationsTableName: 'migrations',
+    synchronize: false,
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
+  }),
+);
